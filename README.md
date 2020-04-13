29aaaa1e1327e99840229722507c77217b230155
Victor Macarios <47126430+vmacarios@users.noreply.github.com>
2020-04-13 11:26:23 +0200

    Added BeansScopeDemoApp to demonstrate that the differences between singleton and prototype scope.

--- README.md
+++ README.md
@@ -5,3 +5,6 @@ MyLoggerConfig defines the Spring log level. In this case, it's set as FINE.
 
 SetterDemoApp demonstrate the use of setters to inject the FortuneService dependency on 
 CricketCoach and also the injection of literal values from a file.
+
+In BeanScopeDemoApp is possible to see that if the bean scope is set as "prototype", 
+each bean will be created in different memory position. 
---
+++ src/main/java/com/macarios/springdemo/BeanScopeDemoApp.java
@@ -0,0 +1,25 @@
+package com.macarios.springdemo;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class BeanScopeDemoApp {
+    public static void main(String[] args) {
+        //load the spring configuration file
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("beanScope-applicationContext.xml");
+
+        //retrieve bean from spring container
+        Coach theCoach = context.getBean("myCoach", Coach.class);
+        Coach alphaCoach = context.getBean("myCoach", Coach.class);
+
+        //check if they are the same bean
+        boolean result = (theCoach == alphaCoach);
+
+        //print out the result
+        System.out.println("Pointing to the same object: " + result);
+        System.out.println("Memory location for \"theCoach\": " + theCoach);
+        System.out.println("Memory location for \"alphaCoach\": " + alphaCoach);
+
+        //close the context
+        context.close();
+    }
+}
